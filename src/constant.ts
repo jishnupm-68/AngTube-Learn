7f849b113fc4e07d4a1c9321ea6f3be2430af014
Jishnu PM <jishnupmbcr68@gail.com>
2025-11-20 19:05:56 +0530

    constant file updating

---
+++ src/constant.ts
@@ -0,0 +1,10 @@
+const  GoogleApiKey = "AIzaSyB-VaB3t1i7hv5ixvRxRYMFGpLI58VxTj8"
+
+export const YOUTUBE_VIDEO_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=in&key="+GoogleApiKey
+
+// export const AUTOCOMPLETE_API = "http://suggestqueries.google.com/complete/search?output=toolbar&hl=en&q="
+
+export const YOUTUBE_SEARCH_AUTOCOMPLETE_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+
+export const CORS_API = "https://cors-anywhere.herokuapp.com/"
+
